addfe11f1dc351adc41266ae319c0951ac4ae9b8
createmath <84016363+createmath@users.noreply.github.com>
2022-08-27 13:46:21 +0530

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,13 @@
+terraform{
+    backend "s3" {
+        bucket = "sn-psl-test"
+        encrypt = true
+        key = "terraform.tfstate"
+        region = "us-east-1"
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    profile = "aws"
+}
